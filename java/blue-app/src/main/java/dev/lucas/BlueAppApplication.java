46bb2dfbef9e57888af6002126f098344ea5851a
Lucas Silva <lcsd.lucas@gmail.com>
2022-09-28 16:39:55 -0300

    Removendo import

--- java/blue-app/src/main/java/dev/lucas/BlueAppApplication.java
+++ java/blue-app/src/main/java/dev/lucas/BlueAppApplication.java
@@ -8,7 +8,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
